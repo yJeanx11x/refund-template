fe7cedb35eee84924932876f3ea4e6819f7620e3
Jean Lucas <j3anlucas010@gmail.com>
2025-03-07 15:47:04 -0300

    cria um objeto com os detalhes na nova despesa,chama a função que irá adicionar o item na lista.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const expense = document.querySelector("#expense")
+const category= document.querySelector("#category")
+const form=document.querySelector("form")
+
 //Tirando as Letras Do input,É deixando apenas números.
 const amount = document.querySelector('#amount')
 amount.oninput= ()=>{
@@ -16,4 +20,32 @@ value=value.toLocaleString('pt-BR', {
 })
 //Retorna o valor Formatado
 return value
+}
+
+//Captura o evento de submit do formulário para obter os valores.
+form.onsubmit=(event)=>{
+//Previne o compotamento padrão de recarregar a página.
+event.preventDefault()
+
+//cria um objeto com os detalhes na nova despesa 
+const newExpense = {
+        id:new Date().getTime(),
+        expense: expense.value,
+        category_id:category.value,
+        category_name:category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at:new Date(),
+}
+//chama a função que irá adicionar o item na lista.
+expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense){
+try {
+       
+        
+} catch (error) {
+        alert('Não Foi possível atualizar a lista de despesas')
+        console.log(error)
+}
 }
