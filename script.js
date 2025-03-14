c2f6501d3115b54ac7b5da73ab92572a1179d258
Jean Lucas <j3anlucas010@gmail.com>
2025-03-14 10:39:54 -0300

    Criar a info da despesa,nome da despesa,categoria da despesa,Adiciona nome e categoria na div das informações da despesa,Adiciona as informações no item,Adiciona o item na lista.

--- script.js
+++ script.js
@@ -54,12 +54,26 @@ function expenseAdd(newExpense) {
     expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
     expenseIcon.setAttribute("alt", newExpense.category_name);
 
-    //Adiciona as informações no item
-    expenseItem.append(expenseIcon);
+    //Criar a info da despesa.
+    const expensedeInfo = document.createElement("div");
+    expensedeInfo.classList.add("expense-info");
+
+    //Cria o nome da despesa.
+    const expenseName = document.createElement("strong");
+    expenseName.textContent = newExpense.expense;
     
+    //Cria a categoria da despesa.
+    const expenseCategory = document.createElement("span");
+    expenseCategory.textContent = newExpense.category_name;
+    
+    //Adiciona nome e categoria na div das informações da despesa.
+      expensedeInfo.append(expenseName, expenseCategory);
+
+    //Adiciona as informações no item
+    expenseItem.append(expenseIcon,expensedeInfo);
+
     //Adiciona o item na lista.
     expenseList.append(expenseItem);
-  
 } catch (error) {
     alert("Não Foi possível atualizar a lista de despesas");
     console.log(error);
